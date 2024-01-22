ad73e7903acb6ba78e895cfd851074d12d42e0ec
Opogy <laguerre.g.g@gmail.com>
2024-01-22 11:34:37 -0500

    hook practice

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import { StyleSheet,Text,View } from 'react-native';
+import React, { useState }from 'react';
+import { StyleSheet,Text,View, Button } from 'react-native';
 
 function App() {
+  const [add, setAdd] = useState(0)
 
+  const addThis = () => {
+    setAdd(add+1)
+  }  
+
+  const resetThis = () => {
+    setAdd(0)
+  }
   return (
     <View style={styles.body}>
-      <Text>Hello</Text>
+      <Text style={styles.text}>Hello Gabriel, how is it going today?</Text>
+      <Text style={styles.text}>You added {add}</Text>
+      <Button style={styles.but} title='Add' onPress={()=>{addThis()}}></Button>
+      <Button title='Reset' onPress={()=>{resetThis()}}></Button>
     </View>
   );
 }
@@ -21,7 +25,18 @@ const styles = StyleSheet.create({
   body: {
     flex: 1,
     backgroundColor: 'blue',
-  }
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 20,
+    margin: 10,
+  },
+  but: {
+    color: 'red',
+    margin: 100,
+  },
 });
 
 export default App;
