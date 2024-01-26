b59699302189f10b42f0c35b75fe8088440243ee
Opogy <laguerre.g.g@gmail.com>
2024-01-26 14:30:54 -0500

    bottom tab navigation

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ScreenA from './ScreenA';
 import ScreenB from './ScreenB';
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-// import { Text,View,StyleSheet, Pressable } from 'react-native';
+
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
+// import { createMaterialBottomTabNavigator} from 'react-native-paper'
 
 // const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator()
+ const Tab = createBottomTabNavigator()
+// const Tab = createMaterialBottomTabNavigator()
 
 function App() {
 
@@ -21,22 +22,38 @@ function App() {
           tabBarIcon: ({focused, size, color})=>{
               let iconName;
               if(route.name==='Screen_A') {
-                iconName='autoprefixer'
+                iconName='autoprefixer';
+                size = focused ? 25:20;
+                // color = focused ? 'red': 'black'
               } else if(route.name==='Screen_B') {
-                iconName='btc'
+                iconName='bomb';
+                size = focused ? 25:20;
+                // color = focused ? 'red': 'black'
               }
               return (
                 <FontAwesome5 
                 name={iconName}
+                size={size}
+                color={color}
                 />
               )
           }
         })
         }
+        tabBarOptions={{
+          activeTintColor: 'blue',
+          inactiveTintColor: 'yellow',
+          activeBackgroundColor: 'red',
+          labelStyle: {
+            fontSize: 15
+          }
+        }}
         >
+        
         <Tab.Screen
           name="Screen_A"
           component={ ScreenA }
+          options={{tabBarBadge: 7}}
         />
         <Tab.Screen
           name="Screen_B"
