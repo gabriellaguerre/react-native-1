7d301e6c00c5bac68a0a810ac1e907979b75f954
Opogy <laguerre.g.g@gmail.com>
2024-02-19 13:56:38 -0500

    some sqlite exercies

--- src/Home.js
+++ src/Home.js
@@ -1,41 +1,122 @@
 import React, { useState, useEffect } from 'react'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { Text, View, StyleSheet, Button } from 'react-native'
+// import AsyncStorage from '@react-native-async-storage/async-storage'
+import {Alert} from 'react-native'
+import { Text, View, StyleSheet, Button, TextInput } from 'react-native'
+import SQLite from 'react-native-sqlite-storage'
+
+const db = SQLite.openDatabase({
+    name: 'MangoDB',
+    location: 'default',
+},
+()=>{},
+error=>{console.log(error)}
+);
+
 
 function Home({navigation}) {
   const [name, setName]= useState('')
+  const [age, setAge] = useState('')
+ 
+
 
   useEffect(()=> {
     getData()
   },[])
 
-  const logout = () => {
+  const logout = async() => {
     try {
-      AsyncStorage.removeItem('UserName')
-      setName('')
-      navigation.navigate('Login')
-
+      // AsyncStorage.removeItem('UserData')
+      db.transaction((tx)=>{
+        tx.executeSql(
+          "DELETE FROM Users",
+          [],
+          ()=>{navigation.navigate('Login')},
+          (error) => {
+            console.log("Error deleting data: ", error)
+          }
+        )
+      })      
     } catch (error) {
-      console.log(error)
+      Alert.alert('Error!', 'An unexpected error occurred.');
     }
   }
 
   const getData = () => {
     try {
-      AsyncStorage.getItem('UserName')
-      .then(value => {
-        if(value != null) {
-          setName(value)
-        } 
+      // AsyncStorage.getItem('UserData')
+      // .then(value => {
+      //   if(value != null) {
+      //     let user = JSON.parse(value)
+      //     setName(user.Name)
+      //     setAge(user.Age)
+      //   } 
+      // })
+      db.transaction((tx)=> {
+        tx.executeSql(
+          "SELECT Name, Age FROM Users",
+          [],
+          (tx, results)=>{
+            let len = results.rows.length;
+            console.log(len, 'IN HOME PAGE')
+            console.log(name, age, 'PPPPPPPPPPP')
+            if(len > 0) {
+              let userName = results.rows.item(0).Name
+              let userAge = results.rows.item(0).Age
+              setName(userName)
+              setAge(userAge)
+            }
+          }
+        )
       })
     } catch (error) {
       console.log(error)
     }
   }
+
+  const updateData = async () => {
+    if(name.length===0){
+        Alert.alert('Warning!', 'Please enter your name')
+
+    } else  {
+        try {
+
+          // let user = {
+          //   Name: name,
+          // }
+          //   await AsyncStorage.mergeItem('UserData', JSON.stringify(user))
+            db.transaction((tx)=>{
+              tx.executeSql(
+                "UPDATE Users SET Name=?",
+                [name],
+                () => { Alert.alert('Success!', 'Your data has been updated.')},
+                error => {console.log(error)}
+                )
+            })
+            
+        } catch (error) {
+            console.log(error)   
+        }
+
+    }
+}
   
     return (
       <View style={styles.body}>
         <Text style={styles.text}>Welcome {name}</Text>
+        <Text style={styles.text}>Your age is {age}</Text>
+        <TextInput 
+          style={styles.input}
+          placeholder='Enter your new name'
+          value={name}
+          onChangeText={(value)=>setName(value)}
+          />
+        <View style={styles.updateButton}>
+        <Button 
+        title='update'
+        color='orange'
+        onPress={updateData}
+        />
+        </View>
         <Button 
         title='Logout'
         color='red'
@@ -55,7 +136,21 @@ function Home({navigation}) {
         fontSize: 40,
         fontWeight: 'bold',
         margin: 10,
-      }
+      },
+      input: {
+        width: 300,
+        borderWidth: 1,
+        borderColor: 'white',
+        borderRadius: 10,
+        backgroundColor: 'white',
+        textAlign: 'center',
+        fontSize: 20,
+        marginTop: 90,
+        marginBottom: 10,
+    },
+    updateButton: {
+      marginBottom: 10,
+    }
 })
 export default Home
 
--- src/Login.js
+++ src/Login.js
@@ -1,20 +1,83 @@
 import { NavigationContainer } from '@react-navigation/native'
-import React, {useState} from 'react'
+import React, {useState,useEffect} from 'react'
 import {Alert} from 'react-native'
-import AsyncStorage from '@react-native-async-storage/async-storage'
+// import AsyncStorage from '@react-native-async-storage/async-storage'
 import { StyleSheet, Text, View, Image, TextInput, Button } from 'react-native'
+import SQLite from 'react-native-sqlite-storage'
 
+const db = SQLite.openDatabase({
+    name: 'MangoDB',
+    location: 'default',
+},
+()=>{},
+error=>{console.log(error)}
+);
 
 function Login ({navigation}) {
     const [name, setName] = useState('')
+    const [age, setAge] = useState('')
+   
+    useEffect(()=>{
+        createTable()
+        getData()
+    },[])
+
+    const createTable = ()=> {
+        db.transaction((tx)=>{
+            tx.executeSql(
+                "CREATE TABLE IF NOT EXISTS "
+                +"Users "
+                +"(ID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Age INTEGER);"
+            )
+        })
+    }
+    const getData = () => {
+        try {
+          // AsyncStorage.getItem('UserData')
+          // .then(value => {
+          //   if(value != null) {
+          //     let user = JSON.parse(value)
+          //     setName(user.Name)
+          //     setAge(user.Age)
+          //   } 
+          // })
+          db.transaction((tx)=> {
+            tx.executeSql(
+              "SELECT Name, Age FROM Users",
+              [],
+              (tx, results)=>{
+                let len = results.rows.length;
+                console.log(len, 'IN LOGIN LEN')
+                if(len > 0) {
+                  let userName = results.rows.item(0).Name
+                  let userAge = results.rows.item(0).Age
+                  setName(userName)
+                  setAge(userAge)
+                }
+              }
+            )
+          })
+        } catch (error) {
+          console.log(error)
+        }
+      }
 
     const setData = async () => {
-        if(name.length===0){
+        if(name.length===0 || age.length===0){
             Alert.alert('Warning!', 'Please enter your name')
 
         } else  {
             try {
-                await AsyncStorage.setItem('UserName', name)
+                // let user = {
+                //     Name: name,
+                //     Age: age,
+                // }
+                // await AsyncStorage.setItem('UserData', JSON.stringify(user))
+                await db.transaction(async (tx)=>{
+                    await tx.executeSql(
+                        "INSERT INTO Users (Name, Age) VALUES('"+name+"',"+age+")"
+                    )
+                })
                 navigation.navigate('Home')
             } catch (error) {
                 console.log(error)   
@@ -28,11 +91,15 @@ function Login ({navigation}) {
         <Image 
             style={styles.logo}
             source={require('../assets/login.png')}/>
-        <Text style={styles.text}>Async Storage</Text>
+        <Text style={styles.text}>SQLite Storage</Text>
         <TextInput 
             style={styles.input}
             placeholder='Enter your name'
             onChangeText={(value)=> setName(value)}/>
+         <TextInput 
+            style={styles.input}
+            placeholder='Enter your age'
+            onChangeText={(value)=> setAge(value)}/>
         <Button 
             title='Login'
             color= 'green'
@@ -56,7 +123,8 @@ const styles = StyleSheet.create({
     text: {
         color: 'white',
         fontWeight: 'bold',
-        fontSize: 20
+        fontSize: 20,
+        marginBottom: 50,
     },
     input: {
         width: 300,
@@ -66,7 +134,6 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         textAlign: 'center',
         fontSize: 20,
-        marginTop: 90,
         marginBottom: 10,
     }
 })
